0132b7a5ba8a41e8812c6560037086d383b78e9f
Joe Xue <joe.xue.mf@gmail.com>
2018-08-28 14:10:27 +0800

    add the missing project

---
+++ src/Masstransit.RabbitMq.Extensions/BusCreator.cs
@@ -0,0 +1,33 @@
+﻿using MassTransit;
+using MassTransit.RabbitMqTransport;
+using System;
+
+namespace Masstransit.RabbitMq.Extensions
+{
+    public static class BusCreator
+    {
+        public static IBusControl CreateBus(Action<IRabbitMqBusFactoryConfigurator, IRabbitMqHost> registrationAction = null)
+        {
+            return Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri(Constants.RabbitMqUri), hst =>
+                {
+                    hst.Username(Constants.UserName);
+                    hst.Password(Constants.Password);
+                });
+
+                registrationAction?.Invoke(cfg, host);
+            });
+        }
+    }
+
+    public class Constants
+    {
+        public const string RabbitMqUri = "rabbitmq://localhost/";
+        public const string UserName = "guest";
+        public const string Password = "guest";
+
+        public const string QueueA = "queue-a";
+        public const string QueueB = "queue-b";
+    }
+}
